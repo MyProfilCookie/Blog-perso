9e0299415ec84af5ec2496684c659498b7ec9cf5
MyProfilCookie <parigopsg24@gmail.com>
2025-02-23 19:23:56 +0100

    ajustement de la page détail produit

--- frontend/app/products/[id]/page.tsx
+++ frontend/app/products/[id]/page.tsx
@@ -7,6 +7,8 @@ import { motion } from "framer-motion";
 import { Card, CardBody } from "@nextui-org/react";
 import { ShoppingCart } from "lucide-react";
 
+import LoadingAnimation from "@/components/loading";
+
 export default function ProductPage() {
   const params = useParams();
   const _id = params?.id; // Vérifie que l'ID est bien passé
@@ -55,10 +57,7 @@ export default function ProductPage() {
     fetchProduct();
   }, [_id]);
 
-  if (loading)
-    return (
-      <p className="text-center text-gray-600">Chargement du produit...</p>
-    );
+  if (loading) return <LoadingAnimation />;
 
   if (!product)
     return (
@@ -69,44 +68,65 @@ export default function ProductPage() {
 
   return (
     <section className="flex flex-col items-center justify-center min-h-screen p-8">
-      <motion.div
+      <motion.h1
         animate={{ opacity: 1, y: 0 }}
+        className="text-4xl font-bold mb-4 text-center"
         initial={{ opacity: 0, y: 20 }}
+        style={{
+          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)",
+        }}
         transition={{ duration: 0.5 }}
-        className="max-w-2xl w-full"
       >
-        <Card className="shadow-xl p-6 bg-white rounded-lg">
-          <CardBody className="flex flex-col items-center text-center">
-            <motion.img
-              src={`${product.imageUrl}`} // 🔹 Vérifie que l'image s'affiche bien
-              alt={product.title}
-              className="w-64 h-64 object-cover rounded-lg mb-6"
-              animate={{ scale: 1 }}
-              initial={{ scale: 0.9 }}
-              transition={{ duration: 0.4 }}
-            />
-            <motion.h1
-              className="text-3xl font-bold text-violet-700"
-              animate={{ opacity: 1, y: 0 }}
-              initial={{ opacity: 0, y: -10 }}
-              transition={{ duration: 0.5 }}
-            >
-              {product.title}
-            </motion.h1>
-            <p className="text-gray-600 mt-4">{product.description}</p>
-            <p className="text-2xl font-semibold text-violet-600 mt-4">
-              {product.price.toFixed(2)}€
-            </p>
-            <motion.button
-              className="mt-6 flex items-center px-6 py-3 bg-violet-600 text-white rounded-lg shadow-md hover:bg-violet-700"
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-            >
-              <ShoppingCart className="mr-2" size={20} /> Ajouter au panier
-            </motion.button>
-          </CardBody>
-        </Card>
-      </motion.div>
+        🛍️ Découvrez plus de détail sur le produit sélectionné :
+      </motion.h1>
+      <Card className="shadow-xl p-6 rounded-lg">
+        <motion.h2
+          animate={{ opacity: 1, y: 0 }}
+          className="text-4xl font-bold mb-4 text-violet-700 text-center "
+          initial={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.5 }}
+        >
+          {product.title}
+        </motion.h2>
+        <CardBody className="flex flex-col items-center text-center">
+          <motion.img
+            alt={product.title}
+            animate={{ scale: 1 }}
+            className="w-64 h-64 object-cover rounded-lg mb-6"
+            initial={{ scale: 0.9 }}
+            src={`${product.imageUrl}`} // 🔹 Vérifie que l'image s'affiche bien
+            transition={{ duration: 0.4 }}
+          />
+          <motion.p
+            animate={{ opacity: 1, y: 0 }}
+            className="text-3xl font-bold text-violet-700"
+            initial={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.5 }}
+          >
+            {/* notation avec des étoiles alignées à droite */}
+            <span className="flex items-center">
+              {Array.from({ length: 5 }, (_, index) => (
+                <span key={index} className="text-yellow-500">
+                  ★
+                </span>
+              ))}
+              {/* Ajout de l'étoile vide */}
+              <span className="text-gray-300">☆</span>
+            </span>
+          </motion.p>
+          <p className="text-gray-600 mt-4">{product.description}</p>
+          <p className="text-2xl font-semibold text-violet-600 mt-4">
+            {product.price.toFixed(2)}€
+          </p>
+          <motion.button
+            className="mt-6 flex items-center px-6 py-3 bg-violet-600 text-white rounded-lg shadow-md hover:bg-violet-700"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <ShoppingCart className="mr-2" size={20} /> Ajouter au panier
+          </motion.button>
+        </CardBody>
+      </Card>
     </section>
   );
 }
