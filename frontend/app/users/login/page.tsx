f624f08b55b043e331d4e89e6181ecb1ffc91e55
MyProfilCookie <parigopsg24@gmail.com>
2025-03-12 13:39:31 +0100

    ajustement visuel pour adapter le site au mode dark pour une meilleure expérience utilisateur + amélioration du menu burger, du mode dark mode

--- frontend/app/users/login/page.tsx
+++ frontend/app/users/login/page.tsx
@@ -61,7 +61,7 @@ export default function Connexion() {
     try {
       const loginData = isEmail ? { email: identifier, password } : { pseudo: identifier, password };
 
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/login`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/users/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(loginData),
