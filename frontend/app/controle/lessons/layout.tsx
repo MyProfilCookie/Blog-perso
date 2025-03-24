f28870cf7d81fd4dc5c089e5f580958664be8ccd
MyProfilCookie <parigopsg24@gmail.com>
2025-03-24 14:00:51 +0100

    correction

--- frontend/app/controle/lessons/layout.tsx
+++ frontend/app/controle/lessons/layout.tsx
@@ -6,8 +6,8 @@ export default function MathLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex items-center justify-center gap-6 py-12 md:py-16">
-      <div className="inline-block p-6 text-center rounded-lg">{children}</div>
+    <section>
+      <div>{children}</div>
     </section>
   );
 }
