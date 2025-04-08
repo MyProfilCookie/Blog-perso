0f21aba0d0d425fbca10fea0bf8a45b47e9a24c5
MyProfilCookie <parigopsg24@gmail.com>
2025-04-08 12:32:18 +0200

    mise à jour du code couleur

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -20,7 +20,20 @@ module.exports = {
     "bg-gray-100", "dark:bg-gray-800",
     "bg-cyan-100", "dark:bg-cyan-800",
     "bg-rose-100", "dark:bg-rose-800",
-    "bg-blue-100", "dark:bg-blue-800"
+    "bg-blue-100", "dark:bg-blue-800",
+
+    "bg-red-400", "dark:bg-red-800",
+    "bg-green-400", "dark:bg-green-800",
+    "bg-yellow-400", "dark:bg-yellow-800",
+    "bg-pink-400", "dark:bg-pink-800",
+    "bg-purple-400", "dark:bg-purple-800",
+    "bg-indigo-400", "dark:bg-indigo-800",
+    "bg-teal-400", "dark:bg-teal-800",
+    "bg-orange-400", "dark:bg-orange-800",
+    "bg-gray-400", "dark:bg-gray-800",
+    "bg-cyan-400", "dark:bg-cyan-800",
+    "bg-rose-400", "dark:bg-rose-800",
+    "bg-blue-400", "dark:bg-blue-800",
   ],
   theme: {
   	extend: {
