5c451305e6ff6c1978b3cc0882136fab459e786c
MyProfilCookie <parigopsg24@gmail.com>
2025-05-01 15:42:37 +0200

    mise à jour

--- frontend/app/shop/PayementPage.tsx
+++ frontend/app/shop/PayementPage.tsx
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY || "", {
-    apiVersion: "2025-02-24.acacia",
+    apiVersion: "2023-10-16",
 });
 
 export default async function handler(
