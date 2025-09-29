8bb90b6fae8513d082c28acc9ab51b76ad1a092b
MyProfilCookie <parigopsg24@gmail.com>
2025-09-29 13:21:48 +0200

    🚨 Fix: Polyfill ULTIME pour Safari - chargé AVANT tout dans _document.tsx

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,48 +4,45 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-        {/* Polyfill ULTRA agressif pour Safari - chargé en premier */}
+        {/* Polyfill ULTIME pour Safari - chargé AVANT tout */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              // Polyfill ULTRA agressif pour Safari
-              (function() {
-                // Définir immédiatement sur window
-                window.requestIdleCallback = function(callback, options) {
-                  var start = Date.now();
-                  return window.setTimeout(function() {
-                    callback({
-                      didTimeout: false,
-                      timeRemaining: function() {
-                        return Math.max(0, 50 - (Date.now() - start));
-                      }
-                    });
-                  }, 1);
-                };
-                
-                window.cancelIdleCallback = function(id) {
-                  window.clearTimeout(id);
-                };
-                
-                // Forcer la définition sur tous les contextes
-                if (typeof global !== 'undefined') {
-                  global.requestIdleCallback = window.requestIdleCallback;
-                  global.cancelIdleCallback = window.cancelIdleCallback;
-                }
-                
-                if (typeof self !== 'undefined') {
-                  self.requestIdleCallback = window.requestIdleCallback;
-                  self.cancelIdleCallback = window.cancelIdleCallback;
-                }
-                
-                // Définir sur l'objet global de manière plus agressive
-                try {
-                  eval('requestIdleCallback = window.requestIdleCallback');
-                  eval('cancelIdleCallback = window.cancelIdleCallback');
-                } catch(e) {}
-                
-                console.log('Safari polyfill ULTRA chargé - requestIdleCallback disponible:', typeof window.requestIdleCallback);
-              })();
+              // Polyfill ULTIME pour Safari - chargé AVANT tout
+              window.requestIdleCallback = window.requestIdleCallback || function(callback, options) {
+                var start = Date.now();
+                return window.setTimeout(function() {
+                  callback({
+                    didTimeout: false,
+                    timeRemaining: function() {
+                      return Math.max(0, 50 - (Date.now() - start));
+                    }
+                  });
+                }, 1);
+              };
+              
+              window.cancelIdleCallback = window.cancelIdleCallback || function(id) {
+                window.clearTimeout(id);
+              };
+              
+              // Forcer la définition sur tous les contextes
+              if (typeof global !== 'undefined') {
+                global.requestIdleCallback = window.requestIdleCallback;
+                global.cancelIdleCallback = window.cancelIdleCallback;
+              }
+              
+              if (typeof self !== 'undefined') {
+                self.requestIdleCallback = window.requestIdleCallback;
+                self.cancelIdleCallback = window.cancelIdleCallback;
+              }
+              
+              // Définir sur l'objet global de manière plus agressive
+              try {
+                eval('requestIdleCallback = window.requestIdleCallback');
+                eval('cancelIdleCallback = window.cancelIdleCallback');
+              } catch(e) {}
+              
+              console.log('Safari polyfill ULTIME chargé - requestIdleCallback disponible:', typeof window.requestIdleCallback);
             `,
           }}
         />
