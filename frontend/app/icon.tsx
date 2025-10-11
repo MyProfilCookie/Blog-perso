c9f01aa9b3cb8a8e685b89d6ffe52b004a1a220e
MyProfilCookie <parigopsg24@gmail.com>
2025-10-11 22:43:12 +0200

    Favicon avec cercle beige et logo puzzle au centre

--- frontend/app/icon.tsx
+++ frontend/app/icon.tsx
@@ -18,22 +18,28 @@ export default function Icon() {
       <svg
         width="32"
         height="32"
-        viewBox="0 0 100 100"
+        viewBox="0 0 200 200"
         xmlns="http://www.w3.org/2000/svg"
         style={{
           background: 'transparent',
         }}
       >
+        {/* Cercle de fond beige */}
+        <circle cx="100" cy="100" r="95" fill="#f5f1e6" />
+        
         {/* Pièce de puzzle bleue */}
         <path
-          d="M 30 15 L 45 15 C 45 10 50 10 50 15 C 50 10 55 10 55 15 L 70 15 C 75 15 75 20 75 25 L 75 40 C 80 40 80 45 75 50 C 80 50 80 55 75 55 L 75 70 C 75 75 70 75 65 75 L 50 75 C 50 80 50 85 45 85 C 40 85 40 80 40 75 L 25 75 C 20 75 20 70 20 65 L 20 50 C 15 50 15 45 20 45 C 15 45 15 40 20 40 L 20 25 C 20 20 25 15 30 15 Z"
+          d="M 60 40 L 85 40 C 85 32 95 32 95 40 C 95 32 105 32 105 40 L 130 40 C 138 40 140 42 140 50 L 140 75 C 148 75 148 85 140 90 C 148 90 148 100 140 100 L 140 125 C 140 133 138 135 130 135 L 105 135 C 105 143 105 153 95 153 C 85 153 85 143 85 135 L 60 135 C 52 135 50 133 50 125 L 50 100 C 42 100 42 90 50 90 C 42 90 42 80 50 75 L 50 50 C 50 42 52 40 60 40 Z"
           fill="#5eb3d6"
           stroke="#1e3a5f"
-          strokeWidth="2"
+          strokeWidth="3"
         />
+        
         {/* Points décoratifs */}
-        <circle cx="85" cy="50" r="4" fill="#e07856" />
-        <circle cx="50" cy="13" r="3" fill="#e07856" />
+        <circle cx="160" cy="95" r="6" fill="#e07856" />
+        <circle cx="40" cy="95" r="5" fill="#e07856" opacity="0.7" />
+        <circle cx="95" cy="28" r="5" fill="#e07856" />
+        <circle cx="95" cy="162" r="5" fill="#e07856" opacity="0.7" />
       </svg>
     ),
     {
